fb2dcd8871706f987161f3923b9362a98b0b12e7
Dave Cassel <dmcassel@gmail.com>
2014-12-17 16:28:22 -0500

    Added copy constructor and destructor; accounted for frequency; commented out logging

--- udf/RangedBucket.cpp
+++ udf/RangedBucket.cpp
@@ -22,16 +22,14 @@ class RangedBucket : public AggregateUDF
 public:
   Bucket *buckets;
   int bucketCount;
-  struct Value {
-    double sum, sum_sq, count;
-    Value() : sum(0), sum_sq(0), count(0) {}
-  } value;
 
 public:
   RangedBucket() {
     bucketCount = 0;
     buckets = 0;
   }
+  RangedBucket(const RangedBucket &rhs);
+  virtual ~RangedBucket();
   AggregateUDF* clone() const { return new RangedBucket(*this); }
   void close() { delete this; }
 
@@ -45,15 +43,32 @@ public:
   void decode(Decoder &d, Reporter &reporter);
 };
 
+RangedBucket::RangedBucket(const RangedBucket &rhs)
+{
+  bucketCount = rhs.bucketCount;
+  buckets = new Bucket[bucketCount];
+  for (int i=0; i<bucketCount; i++) {
+    buckets[i].min = rhs.buckets[i].min;
+    buckets[i].max = rhs.buckets[i].max;
+    buckets[i].count = rhs.buckets[i].count;
+  }
+}
+
+RangedBucket::~RangedBucket()
+{
+  if (buckets)
+    delete[] buckets;
+}
+
 void RangedBucket::start(Sequence &arg, Reporter &r)
 {
-  std::string msg;
+  // std::string msg;
   if (arg.done()) {
     r.error("First argument must be bucket count -- not found.");
   } else {
     arg.value(bucketCount);
-    msg = "start: Bucket count is " + std::to_string(bucketCount);
-    r.log(Reporter::Info, msg.c_str());
+    // msg = "start: Bucket count is " + std::to_string(bucketCount);
+    // r.log(Reporter::Info, msg.c_str());
     buckets = new Bucket[bucketCount];
     for (int i=0; i<bucketCount; i++) {
       if (arg.done()) {
@@ -72,18 +87,18 @@ void RangedBucket::start(Sequence &arg, Reporter &r)
 
 void RangedBucket::finish(OutputSequence &os, Reporter &reporter)
 {
-  std::string msg;
+  // std::string msg;
 
-  msg = "finish: bucketCount=" + std::to_string(bucketCount);
-  reporter.log(Reporter::Info, msg.c_str());
+  // msg = "finish: bucketCount=" + std::to_string(bucketCount);
+  // reporter.log(Reporter::Info, msg.c_str());
 
   os.startMap();
 
   for (int i=0; i<bucketCount; i++) {
     std::string key;
     key += std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max);
-    msg = "finish: key=" + key + "; value=" + std::to_string(buckets[i].count);
-    reporter.log(Reporter::Info, msg.c_str());
+    // msg = "finish: key=" + key + "; value=" + std::to_string(buckets[i].count);
+    // reporter.log(Reporter::Info, msg.c_str());
     os.writeMapKey(key.c_str());
     os.writeValue(buckets[i].count);
   }
@@ -94,18 +109,19 @@ void RangedBucket::finish(OutputSequence &os, Reporter &reporter)
 void RangedBucket::map(TupleIterator &values, Reporter &reporter)
 {
   int lo, hi;
-  std::string msg;
+  // std::string msg;
 
   for(; !values.done(); values.next()) {
     if(!values.null(0)) {
       values.value(0, lo);
       values.value(1, hi);
-      msg = "map: lo=" + std::to_string(lo) + "; hi=" + std::to_string(hi);
-      reporter.log(Reporter::Info, msg.c_str());
+      // msg = "map: lo=" + std::to_string(lo) + "; hi=" + std::to_string(hi);
+      // reporter.log(Reporter::Info, msg.c_str());
       for (int i=0; i<bucketCount; i++) {
         if (lo <= buckets[i].max && hi >= buckets[i].min) {
-          ++buckets[i].count;
-          reporter.log(Reporter::Info, ("Bucket " + std::to_string(i) + " now has " + std::to_string(buckets[i].count)).c_str());
+          buckets[i].count += values.frequency();
+          // reporter.log(Reporter::Info, ("Bucket " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) +
+          //   " now has " + std::to_string(buckets[i].count)).c_str());
         }
       }
     }
@@ -115,35 +131,35 @@ void RangedBucket::map(TupleIterator &values, Reporter &reporter)
 void RangedBucket::reduce(const AggregateUDF *_o, Reporter &reporter)
 {
   const RangedBucket *o = (const RangedBucket*)_o;
-  std::string msg;
+  // std::string msg;
   for (int i=0; i<bucketCount; i++) {
-    msg = "reduce: " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) + "-> " +
-      std::to_string(buckets[i].count) + " + " + std::to_string(o->buckets[i].count);
-    reporter.log(Reporter::Info, msg.c_str());
+    // msg = "reduce: " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) + "-> " +
+    //   std::to_string(buckets[i].count) + " + " + std::to_string(o->buckets[i].count);
+    // reporter.log(Reporter::Info, msg.c_str());
     buckets[i].count += o->buckets[i].count;
   }
 }
 
 void RangedBucket::encode(Encoder &e, Reporter &reporter)
 {
-  reporter.log(Reporter::Info, "calling encode");
+  // reporter.log(Reporter::Info, "calling encode");
   e.encode(bucketCount);
   for (int i=0; i<bucketCount; i++) {
     e.encode(&buckets[i], sizeof(Bucket));
-    reporter.log(Reporter::Info,
-      ("encode: " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) + "-> " + std::to_string(buckets[i].count)).c_str());
+    // reporter.log(Reporter::Info,
+    //   ("encode: " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) + "-> " + std::to_string(buckets[i].count)).c_str());
   }
 }
 
 void RangedBucket::decode(Decoder &d, Reporter &reporter)
 {
-  reporter.log(Reporter::Info, "calling decode");
+  // reporter.log(Reporter::Info, "calling decode");
   d.decode(bucketCount);
   buckets = new Bucket[bucketCount];
   for (int i=0; i<bucketCount; i++) {
     d.decode(&buckets[i], sizeof(Bucket));
-    reporter.log(Reporter::Info,
-      ("decode: " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) + "-> " + std::to_string(buckets[i].count)).c_str());
+    // reporter.log(Reporter::Info,
+    //   ("decode: " + std::to_string(buckets[i].min) + "-" + std::to_string(buckets[i].max) + "-> " + std::to_string(buckets[i].count)).c_str());
   }
 }
 
